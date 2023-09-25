a07b4a5b267514c1ae050af0f83d24743f9cfc4f
ychekirine <72571309+ychekirine@users.noreply.github.com>
2023-09-25 14:41:31 +0200

    Adding and editing product and ProductCategory entities (#3)

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/youSafeSevices/ecommerce/entity/Product.java
@@ -0,0 +1,50 @@
+package com.youSafeSevices.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    /* We could add @Column(name = "id") but it will do it automatically (I think) */
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="category_id", nullable = false)
+    private ProductCategory category;
+
+    private String sku;
+
+    private String name;
+
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    private boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStrock;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @Column(name = "date_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+
+}
