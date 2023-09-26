6e2dc5fce07c4e4f988375bf61baeb696f612268
Youcef Chekirine <youcef.chekirine@gmail.com>
2023-09-26 13:57:04 +0200

    Implementing product service and product-list component

---
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../common/product';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  private apiUrl = "http://localhost:8080/api/products";
+  
+
+  getData(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.apiUrl).pipe(
+      map(Response => Response._embedded.products));
+  }
+
+}
+
+interface GetResponse{
+  _embedded: {
+    products: Product[];
+  }
+}
